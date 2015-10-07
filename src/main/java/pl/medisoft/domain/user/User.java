2a213f1b6350843577d50e970b51ab5478a14bc4
Mariusz Batyra <mbatyra@wp.eu>
2015-10-07 20:15:37 +0200

    Example entity

--- src/main/java/pl/medisoft/domain/user/User.java
+++ src/main/java/pl/medisoft/domain/user/User.java
@@ -5,10 +5,94 @@
  */
 package pl.medisoft.domain.user;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Mariusz Batyra
  */
-public class User {
-    // TODO user entity
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    private Long id;
+    private String login;
+    private String name;
+    private String surname;
+    private String passhash;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String login, String name, String surname, String passhash, String email) {
+        this.id = id;
+        this.login = login;
+        this.name = name;
+        this.surname = surname;
+        this.passhash = passhash;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "surname")
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Column(name = "passhash")
+    public String getPasshash() {
+        return passhash;
+    }
+
+    public void setPasshash(String passhash) {
+        this.passhash = passhash;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
